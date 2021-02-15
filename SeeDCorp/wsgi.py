f419d46fce187d54754913fbc3b4958d48f71688
SeeDCharlie <hellocarloscastillo@gmail.com>
2021-02-15 09:43:44 -0500

    asdas

--- SeeDCorp/wsgi.py
+++ SeeDCorp/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/home/seedch/SeeDCorp/SeeDCorp')
+
+# add the virtualenv site-packages path to the sys.path
+sys.path.append('/home/seedch/venvSeedCorp/lib/python3.9/site-packages')
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SeeDCorp.settings')
 
 application = get_wsgi_application()
