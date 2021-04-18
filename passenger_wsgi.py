cfe4568f41418709bbe55d83dea9e550bc662ff4
SeeDCharlie <hellocarloscastillo@gmail.com>
2021-04-17 22:39:03 -0500

    asdasd

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -7,12 +7,12 @@ if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 
 cwd = os.getcwd()
 sys.path.append(cwd)
-sys.path.append(cwd + '/lavadotanques')  #You must add your project here
+sys.path.append(cwd + '/SeeDCorp')  #You must add your project here
 
 sys.path.insert(0,'/home/seedch/venvSeedCorp/bin')
 sys.path.insert(0,cwd+'/home/seedch/venvSeedCorp/lib/python3.9/site-packages')
 
-os.environ['DJANGO_SETTINGS_MODULE'] = "lavadotanques.settings"
+os.environ['DJANGO_SETTINGS_MODULE'] = "SeeDCorp.settings"
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
